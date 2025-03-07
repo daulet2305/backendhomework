bce637aeab17c8c066bb26d1935b106ba2725e0f
Daulet Khairushev <daulet2305@mail.ru>
2025-03-08 02:27:47 +0500

    Первый коммит: инициализация проекта и добавление сериализаторов

---
+++ subjects/urls.py
@@ -0,0 +1,8 @@
+# subjects/urls.py
+from django.urls import path
+from .views import CourseList, CourseDetail
+
+urlpatterns = [
+    path('courses/', CourseList.as_view(), name='course-list'),
+    path('courses/<int:pk>/', CourseDetail.as_view(), name='course-detail'),
+]
---
+++ subjects/views.py
@@ -0,0 +1,13 @@
+# subjects/views.py
+from rest_framework import generics
+from .models import Course
+from .serializers import CourseSerializer
+
+class CourseList(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
